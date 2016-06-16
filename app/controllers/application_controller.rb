e38881df368a65df539b55b8a852688082b75945
Javier Romero <deromero@gmail.com>
2016-06-16 16:08:10 -0500

    se incluye validación para rol de administrador y politica de usuarios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protect_from_forgery
   protect_from_forgery with: :exception
   before_filter :set_locale
@@ -15,4 +17,12 @@ class ApplicationController < ActionController::Base
   def default_url_options(options={})
     { locale: I18n.locale }.merge options
   end
+
+  private
+
+    def user_not_authorized
+      flash[:alert] = "Access Denied"
+      redirect_to (request.referrer || root_path)
+    end
+
 end
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,13 @@
+class UserPolicy < ApplicationPolicy
+  attr_reader :current_user, :model
+
+  def initialize(current_user, model)
+    @current_user = current_user
+    @user = model
+  end
+
+  def index?
+    @current_user.admin?
+  end
+
+end
