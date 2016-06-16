4636ae4bf2d5dd1fa3a7eb8a4002cc15ce9f5e64
Javier Romero <deromero@gmail.com>
2016-06-16 15:59:17 -0500

    se agrega controller de usuarios y se incluye roles para un usuario

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :categories
     resources :projects
     devise_for :users
+    resources :users
   end
 
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,11 @@ FactoryGirl.define do
     #name "Test User"
     email "test@example.com"
     password "please123"
+
+    trait :admin do
+      role 'admin'
+    end
+
   end
 
 end
